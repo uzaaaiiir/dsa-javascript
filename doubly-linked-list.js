229469731d7cfe81f2dbe8a8db870b91d97a96f0
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-10-04 22:11:11 -0400

    Doubly-Linked List: shift, unshift, get

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -21,6 +21,11 @@ class DoublyLinkedList {
         this.length = 1;
     }
 
+    /**
+     * Method adds a node to the end of doubly-linked list.
+     * @param {any} value
+     * @returns DoublyLinkedList
+     */
     push(value) {
         // Create new Node
         const node = new Node(value);
@@ -39,6 +44,10 @@ class DoublyLinkedList {
         return this;
     }
 
+    /**
+     * Method removes a node from the end of the doubly-linked list.
+     * @returns Node
+     */
     pop() {
         // if empty, return undefined
         if (!this.length) return undefined;
@@ -58,6 +67,96 @@ class DoublyLinkedList {
         return temp;
     }
 
+    /**
+     * Method adds a node to the beginning of a doubly-linked list.
+     * @param {any} value
+     * @returns DoublyLinkedList
+     */
+    unshift(value) {
+        const node = new Node(value);
+
+        if (!this.head) {
+            // If empty list, return undefined
+            this.head = this.tail = node;
+        } else {
+            // Adjust pointers
+            node.next = this.head;
+            this.head.prev = node;
+            this.head = node;
+        }
+
+        this.length++;
+        return this;
+    }
+
+    /**
+     * Method removes a node from the beginning of a doubly-linked list.
+     * @returns Node
+     */
+    shift() {
+        // empty list
+        if (!this.head) return undefined;
+
+        const temp = this.head;
+        if (this.length === 1) {
+            this.head = this.tail = null;
+        } else {
+            this.head = this.head.next;
+            this.head.prev = null;
+            temp.next = null;
+        }
+
+        this.length--;
+        return temp;
+    }
+
+    /**
+     * Method retrieves the Node at the specified index.
+     * @param {int} index
+     * @returns Node
+     */
+    get(index) {
+        if (!this.head || index >= this.length) return undefined;
+
+        let temp = this.head;
+        for (let i = 0; i < index - 1; i++) {
+            temp = temp.next;
+        }
+
+        return temp.value;
+    }
+
+    /**
+     * Method replaces a value of a Node at the specified index.
+     * @param {int} index
+     * @param {any} value
+     * @returns DoublyLinkedList
+     */
+    set(index, value) {
+        if (!this.head) return undefined;
+        return this;
+    }
+
+    /**
+     * Method inserts a Node with the given value at the specified index.
+     * @param {int} index
+     * @param {int} value
+     * @returns DoublyLinkedList
+     */
+    insert(index, value) {
+        if (index < this.length) return undefined;
+        return this;
+    }
+
+    /**
+     * Methods removes and returns the Node at the specified index.
+     * @param {int} index
+     * @returns Node
+     */
+    remove(index) {
+        if (index < this.head) return undefined;
+    }
+
     print() {
         let node = this.head;
         for (let i = 0; i < this.length; i++) {
@@ -66,14 +165,13 @@ class DoublyLinkedList {
             );
             node = node.next;
         }
+
+        console.log(`Length: ${this.length}`);
     }
 }
 
 let list = new DoublyLinkedList(7);
 list.push(12);
-list.push(1);
-list.pop();
-list.pop();
-list.pop();
-list.print();
-console.log(list);
+list.unshift(343);
+console.log(list.get(2));
+// list.print();
