a908bd72dd2f54b63a1fcbfe5d471ceb46533cdd
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-10-06 08:54:52 -0400

    Stack: push, pop

--- stack.js
+++ stack.js
@@ -1,26 +1,76 @@
-const { LinkedList } = require("./linked-list");
-
-class Stack {
-    stack;
+class Node {
+    value;
+    next;
 
     constructor(value) {
-        this.stack = new LinkedList(value);
-    }
-
-    push(value) {
-        this.stack.push(value);
-    }
-
-    pop() {
-        this.stack.pop();
-    }
-
-    print() {
-        this.stack.printList();
+        this.value = value;
+        this.next = null;
     }
 }
 
-const list = new Stack(5);
-list.push(33);
-list.pop();
-list.print();
+/**
+ * Class representing a Stack.
+ */
+class Stack {
+    top;
+    length;
+
+    constructor(value) {
+        if (!(value === undefined || value === null)) {
+            this.top = new Node(value);
+            this.length = 1;
+        }
+    }
+
+    /**
+     * Adds an item to the top of the stack
+     * @param {any} value
+     * @returns Stack
+     */
+    push(value) {
+        const node = new Node(value);
+
+        if (!this.top) {
+            this.top = node;
+        } else {
+            node.next = this.top;
+            this.top = node;
+        }
+
+        this.length++;
+        return this;
+    }
+
+    /**
+     * Removes an item from the top of the stack
+     * @returns Node - which is removed
+     */
+    pop() {
+        if (!this.top || this.length === 0) return undefined;
+
+        const temp = this.top;
+        this.top = this.top.next;
+        temp.next = null;
+
+        this.length--;
+        return temp;
+    }
+
+    print() {
+        let curr = this.top;
+        for (let i = 0; i < this.length; i++) {
+            console.log(`Current ${curr.value}, next ${curr?.next?.value}`);
+            curr = curr.next;
+        }
+
+        console.log(`Length ${this.length}`);
+    }
+}
+
+const myStack = new Stack(5);
+myStack.push(7);
+myStack.pop();
+myStack.pop();
+myStack.push(12);
+myStack.pop();
+myStack.print();
