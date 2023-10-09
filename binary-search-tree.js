d5019f6398947cf5a2edb3f1dbf7de8601ad4e08
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-10-09 15:42:24 -0400

    BST: Description

---
+++ binary-search-tree.js
@@ -0,0 +1,18 @@
+/**
+ * Trees
+ *
+ * Binary Tree
+ * - Each node has two children at most: left and right
+ * - Full Tree: each node either has two children or 0 children
+ * - Perfect Tree: every line is perfectly filled across that height of the tree
+ * - Complete Tree: lowest level is filled from the left most side, and the rest are perfect
+ *
+ * - Parent Node
+ * - Child Node
+ * - Leaf Node - node with no children
+ *
+ * Binary Search Tree (BST)
+ * - If value is greater than parent node, it goes to the right, otherwise to the left.
+ * - Perfect Tree has 2^n -1 nodes - where n is the height
+ * - O(logn) = O(h) to find, add, remove [divide and conquer]
+ */
