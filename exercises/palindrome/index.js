15a8aaaf2bd56b00aae74b797444177d9ac6adfc
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-10-24 18:36:58 -0400

    Exercises: Palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,15 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    /**
+     * Alternate solution
+     */
+    // return str.split("").every((char, i) => {
+    //     return char === str[str.length - i - 1];
+    // });
+
+    return str === str.split("").reverse().join("");
+}
 
 module.exports = palindrome;
